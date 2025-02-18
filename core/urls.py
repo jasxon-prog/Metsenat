af3c2708856a0c95662c202b114f03cbdab15958
jasxon <jasurbekx98@gmail.com>
2025-02-18 20:01:52 +0500

    metsenat

--- core/urls.py
+++ core/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from app.views import SponsorListCreateAPIView, SponsorDetailAPIView, StudentListCreateAPIView, StudentDetailAPIView, DashboardAPIView, StudentSponsorCreateAPIView, StudentSponsorUpdateAPIView, DashboardGraphAPIView
 
@@ -30,3 +32,7 @@ urlpatterns = [
     path('dashboard/', DashboardAPIView.as_view()),
     path('dashboard/graph/', DashboardGraphAPIView.as_view()),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
